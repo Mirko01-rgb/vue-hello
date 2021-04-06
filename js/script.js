bf21a85223bb30b786ce4b40eaadd60a63904f0b
Mirko <mirkosbaglia2001@gmail.com>
2021-04-06 14:53:04 +0200

    Esercizio terminato

--- js/script.js
+++ js/script.js
@@ -1,2 +1,26 @@
 // GOAL: stampare a schermo il contenuto di una variabile dentro un h1
 // BONUS: utilizzare un data come nome del file immagine in un tag img
+
+function initVue(){
+ new Vue({
+   el: '#app',
+   data : {
+    'text':'ciao',
+    'immagine' : './img/Roma.jpg'
+  }
+
+ });
+}
+
+
+
+
+
+
+
+
+
+function init(){
+  initVue();
+}
+document.addEventListener('DOMContentLoaded', init);
